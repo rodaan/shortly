ea88498fea3083c3d5339639200b2e8e98315ba7
Hack Reactor Students <accounts@hackreactor.com>
2016-05-21 11:49:02 -0700

    Add bootstrap navbar

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,10 +2,20 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
+  $scope.showMessage = false;
+  $scope.submitting = function() {
+    if ($scope.showMessage) {
+      $scope.showMessage = false;
+    } else {
+      $scope.showMessage = true;
+    }
+  };
   $scope.addLink = function () {
     Links.addOne($scope.link)
       .then(function(data) {
         console.log('=======added link ======', data);
+        $scope.submitting();
+        $scope.link.url = '';
       })
       .catch(function(error) {
         console.error('error at addLink', error);
