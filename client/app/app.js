152ac58824f1e7db82dd075b5085cfe05e7db3bb
Hack Reactor Students <accounts@hackreactor.com>
2016-05-20 20:55:05 -0700

    Unautheticated users are sent to the signin page

--- client/app/app.js
+++ client/app/app.js
@@ -22,6 +22,9 @@ angular.module('shortly', [
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
+    })
+    .otherwise({
+      redirectTo: '/links'
     });
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
@@ -53,7 +56,9 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+    console.log('this is next', next);
+    // next.$$route && next.$$route.authenticate &&
+    if (next.$$route && !Auth.isAuth()) {
       $location.path('/signin');
     }
   });
